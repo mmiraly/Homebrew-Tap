163eb7b698f9e6fd53041486bbee88142b5503e3
Mira <coolboy1384@gmail.com>
2026-01-31 17:05:11 -0500

    update cawa to v1.0.0

--- Formula/cawa.rb
+++ Formula/cawa.rb
@@ -2,7 +2,8 @@ class Cawa < Formula
   desc "Context-Aware Workspace Automation"
   homepage "https://github.com/mmiraly/cawa"
   url "https://github.com/mmiraly/cawa/releases/download/v1.0.0/cs-v1.0.0-darwin-arm64.tar.gz"
-  sha256 "d86b099c6da3556be2e9636c03576995a1f0fc1454cac0d42b78a0030e2fd5d4"
+  version "v1.0.0"
+  sha256 "ce9597b16bc4642651287b30ca11ead8ff0b0a3e4d89e9903b26a9281ed84a3c"
   license "GPL-3.0"
 
   # Binary is precompiled for Apple Silicon
