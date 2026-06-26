bf173092a41928bdf7dc895e350665bec05efbf4
Mahbod Miraly <mahbod.miraly@gmail.com>
2026-06-25 20:56:29 -0400

    update nuked to v1.3.1

--- Formula/nuked.rb
+++ Formula/nuked.rb
@@ -1,26 +1,26 @@
 class Nuked < Formula
   desc "Nuke stale project dependency folders."
   homepage "https://github.com/mmiraly/nukeD"
-  version "v1.0.1"
+  version "v1.3.1"
   license "GPL-3.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/mmiraly/nukeD/releases/download/v1.0.1/nuked-v1.0.1-darwin-amd64.tar.gz"
-      sha256 "bcfb7e0f5c0628b82eaa4c8ec1da80f9e4dc1c6ae4abde300b61b00292f9e090"
+      url "https://github.com/mmiraly/nukeD/releases/download/v1.3.1/nuked-v1.3.1-darwin-amd64.tar.gz"
+      sha256 "cf2d93e616322a0eb0f65bca729f816d31e0df530b2a08d723214cb80d76e206"
     else
-      url "https://github.com/mmiraly/nukeD/releases/download/v1.0.1/nuked-v1.0.1-darwin-arm64.tar.gz"
-      sha256 "68480ac9c838e08ba1f032ef9d714d31c23e228bb9e0bfe4e25510f8d509facc"
+      url "https://github.com/mmiraly/nukeD/releases/download/v1.3.1/nuked-v1.3.1-darwin-arm64.tar.gz"
+      sha256 "a557f12c01033d3b34a26f54940b9bc0a6df6f618bc6b910d00d5ab2317f6d69"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/mmiraly/nukeD/releases/download/v1.0.1/nuked-v1.0.1-linux-amd64.tar.gz"
-      sha256 "b065605d969b973bd7b4202ecf1a4134da800d1457dc91beb4599a6932f155e8"
+      url "https://github.com/mmiraly/nukeD/releases/download/v1.3.1/nuked-v1.3.1-linux-amd64.tar.gz"
+      sha256 "2cc5bed8425a4928ea22bbb1c73d339c280dbf616890966f743d97d1363a4e48"
     else
-      url "https://github.com/mmiraly/nukeD/releases/download/v1.0.1/nuked-v1.0.1-linux-arm64.tar.gz"
-      sha256 "a5876b9c77a8fadcc57174d33a29690a99b26c5cd6ebe3f9c7d2528c51451a19"
+      url "https://github.com/mmiraly/nukeD/releases/download/v1.3.1/nuked-v1.3.1-linux-arm64.tar.gz"
+      sha256 "9807198f366035eaadf2371b3fe15da53c257eea16b7f000afa466ea8f7d71b6"
     end
   end
 
